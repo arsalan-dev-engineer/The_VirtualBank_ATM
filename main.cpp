42257491a1b1719f81a5c63cb579fa70184e2fbc
Arsalan <50211547+Azzy001@users.noreply.github.com>
2022-02-09 20:40:13 +0000

    Update main.cpp
    Added all options, users can select the following options shown in the menu function.

--- main.cpp
+++ main.cpp
@@ -2,8 +2,9 @@
 # include <string>
 using namespace std;
 
-
+// menu function
 void menu(){
+    // menu
     cout << endl << "------ VirtualBank ATM -----------------------------------" << endl << endl;
     
     cout << "Welcome to your VirtualBank ATM" << endl;
@@ -15,23 +16,28 @@ void menu(){
     cout << endl << "------ VirtualBank ATM -----------------------------------" << endl << endl;
 }
 
+// main function
 int main(){
     double balance = 0;
     int choice;
     do {
+    // call menu function
     menu();
     cout << "Enter your choice: ";
-    // choice/options to navigate
+    // user can choose an option shown in menu function
     cin >> choice;
     // command clears console
     system("cls");
     
+    // switch will allow user to select the following case
     switch(choice){
         case 1:
+            // case 1 shows total balance
             cout << "Total Balance:" << " $ " << balance << endl;
             break;
 
         case 2:
+            // case 2 lets user put money into account
             cout << "Deposit Amount: $ ";
             double deposit_ttl;
             cin >> deposit_ttl;
@@ -39,9 +45,11 @@ int main(){
             break;
 
         case 3:
+            // case 3 lets user take money out of account
             cout << "Withdray Amount: $ ";
             float withdraw_ttl;
             cin >> withdraw_ttl;
+            // condition
             if (withdraw_ttl <= balance){
                 balance -= withdraw_ttl;
             } else {
@@ -49,6 +57,7 @@ int main(){
             break;
             }
         }
+    // if 4 selected, system will exit
     } while (choice != 4);
         cout << "Exiting ATM, Goodbye!" << endl << endl;
 }
