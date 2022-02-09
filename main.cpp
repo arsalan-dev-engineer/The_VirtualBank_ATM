070a31d0d86953020ebff6dc95255339f18a2413
Arsalan <50211547+Azzy001@users.noreply.github.com>
2022-02-09 20:06:11 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,13 +12,43 @@ void menu(){
     cout << "\t3. Withdraw Money" << endl;
     cout << "\t4. Exit ATM" << endl;
 
-    int choice;
-    cout << "Enter your choice: ";
-    cin >> choice;
-
     cout << endl << "------ VirtualBank ATM -----------------------------------" << endl << endl;
 }
 
 int main(){
+    double balance = 0;
+    int choice;
+    do {
     menu();
+    cout << "Enter your choice: ";
+    // choice/options to navigate
+    cin >> choice;
+    // command clears console
+    system("cls");
+    
+    switch(choice){
+        case 1:
+            cout << "Total Balance:" << " $ " << balance << endl;
+            break;
+
+        case 2:
+            cout << "Deposit Amount: $ ";
+            double deposit_ttl;
+            cin >> deposit_ttl;
+            balance += deposit_ttl;
+            break;
+
+        case 3:
+            cout << "Withdray Amount: $ ";
+            float withdraw_ttl;
+            cin >> withdraw_ttl;
+            if (withdraw_ttl <= balance){
+                balance -= withdraw_ttl;
+            } else {
+                cout << "Unable to complete Withdraw" << endl;
+            break;
+            }
+        }
+    } while (choice != 4);
+        cout << "Exiting ATM, Goodbye!" << endl << endl;
 }
